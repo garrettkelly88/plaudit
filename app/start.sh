88c7b9ec68719b78589df4e4c9301949ac871ff0
root <garrett.kelly88@gmail.com>
2021-02-12 04:58:25 +0000

    20210211

--- app/start.sh
+++ app/start.sh
@@ -83,10 +83,9 @@ rm -f /run/apache2/httpd.pid
 rm -Rf /app/public/*
 rm -Rf /app/public/.*
 
-git -C /app/public/ clone -b app https://github.com/garrettkelly88/plaudit.git ./
-mkdir /app/public/includes
-git -C /app/public/includes clone -b swaggerhub https://github.com/garrettkelly88/plaudit.git ./
-cd /app/public/
+git -C /app/public/app clone -b app https://github.com/garrettkelly88/plaudit.git ./
+git -C /app/public/swagger clone -b swaggerhub https://github.com/garrettkelly88/plaudit.git ./
+cd /app/public/app
 composer require swagger/server-bundle:dev-main
 #ln -s /app/images /app/public
 
